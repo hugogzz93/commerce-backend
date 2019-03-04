cfc49af517859775705c74d0f4f11b67f126d1e2
Hugo González <pinelo93@gmail.com>
2019-03-04 13:34:30 -0600

    Adds vendors relation to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
+  has_many :vendors, through: :products, source: :user
   validates :name, presence: true, uniqueness: true
 end
