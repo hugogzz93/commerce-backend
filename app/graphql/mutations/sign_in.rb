0b06a996f5a33d2d977b554523ef54b9970bde3f
Hugo González <pinelo93@gmail.com>
2019-03-14 18:08:44 -0600

    Adds password validation (lol)

--- app/graphql/mutations/sign_in.rb
+++ app/graphql/mutations/sign_in.rb
@@ -1,16 +1,17 @@
 module Mutations
   class SignIn < GraphQL::Schema::Mutation
     field :auth_token, String, null: true
-    
+
     argument :email, String, required: true
     argument :password, String, required: true
 
     def resolve(email:, password:)
-      user = User.find_by_email(email)
-      return nil unless user
+      user = User.find_for_authentication(email: email)
+      return nil unless user 
+      return nil unless user.valid_password? password
       auth_token = Devise.friendly_token
       user.update! authentication_token: auth_token
-      {auth_token: auth_token}
+      { auth_token: auth_token }
     end
   end
 end
